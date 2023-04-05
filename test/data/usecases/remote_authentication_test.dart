454a7d6699a4c80ece50a1d3cf9e42da4098bc7f
RafaelTiTa <127523426+RafaelTiTa@users.noreply.github.com>
2023-04-05 09:08:16 -0300

    move params into stup method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -13,19 +13,16 @@ void main() {
   RemoteAuthentication sut;
   HttpClient httpClient;
   String url;
-
+  AuthenticationParams params;
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    params = AuthenticationParams(
+        email: faker.internet.email(), password: faker.internet.password());
   });
 
   test('Should call HttpClient with correct values', () async {
-    final params = AuthenticationParams(
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    );
-
     await sut.auth(params);
 
     verify(httpClient.request(
@@ -40,9 +37,6 @@ void main() {
             method: anyNamed('method'),
             body: anyNamed('body')))
         .thenThrow(HttpError.unauthorized);
-
-    final params = AuthenticationParams(
-        email: faker.internet.email(), password: faker.internet.password());
 
     final future = sut.auth(params);
 
